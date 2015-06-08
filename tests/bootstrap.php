02890c214d06f36f9a9e83ab8af8bee1e386ca30
Martijn <martijn@vanderlee.com>
2015-06-08 20:55:35 +0200

    Travis

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+	require_once 'susort.php';
+	require_once 'suksort.php';
+	require_once 'suasort.php';
+	require_once 'snatsort.php';
+	require_once 'snatcasesort.php';
+	require_once 'sasort.php';
+	require_once 'sarsort.php';
+
+
+
+
+
