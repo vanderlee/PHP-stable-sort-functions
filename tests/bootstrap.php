21b988b15427097a7193843cec64aedb59c724ed
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-06-25 20:43:30 +0700

    Fix phpunit namespace by aliasing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+class_alias('PHPUnit\\Framework\\TestCase', 'PHPUnit_Framework_TestCase');
+
 require_once __DIR__ . '/../classes/StableSort.php';
 
 require_once __DIR__ . '/../functions.php';
