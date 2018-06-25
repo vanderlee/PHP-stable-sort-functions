77accd13dccf79248f16d955fb2e2d2e65fe3fdb
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-06-25 20:35:01 +0700

    Fix test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,5 @@
 <?php
 
-require_once 'classes/StableSort.php';
+require_once __DIR__ . '/../classes/StableSort.php';
 
-require_once 'functions/susort.php';
-require_once 'functions/suksort.php';
-require_once 'functions/suasort.php';
-require_once 'functions/snatsort.php';
-require_once 'functions/snatcasesort.php';
-require_once 'functions/sasort.php';
-require_once 'functions/sarsort.php';
+require_once __DIR__ . '/../functions.php';
